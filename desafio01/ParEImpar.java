f0687c706581ec42b0c1c0fa2daebdefe18aa583
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-07-28 15:36:34 -0300

    Pares e ímpares

---
+++ desafio01/ParEImpar.java
@@ -0,0 +1,38 @@
+package desafio01;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ParEImpar {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        List<Integer> pares = new ArrayList<>();  //Lista números pares
+        List<Integer> impares = new ArrayList<>();  //Lista números ímpares
+
+        for (int i = 0; i < 10; i++) {
+            int valor = scanner.nextInt();
+
+            if (valor % 2 == 0) {
+                pares.add(valor);
+            } else {
+                impares.add(valor);
+            }
+
+        }
+        Collections.sort(pares); //Pares em ordem crescente
+        Collections.sort(impares, Collections.reverseOrder()); //Ímpares em ordem decrescente
+    
+        for(int par : pares) {
+            System.out.println(par);
+        }
+          for(int impar : impares) {
+            System.out.println(impar);
+        }
+    }
+}
